80120edfe576c55cb42ae2328d8a864cf1740c04
ry-dgel <rydgel.code@gmail.com>
2017-11-20 15:14:16 -0500

    Translated main loop

--- self-phase.jl
+++ self-phase.jl
@@ -45,12 +45,12 @@ const λ_max = 1500
 ###################
 # General Physics Constants
 const c  = 299792458        # Speed of light     m/s
-const ħ = 1.0545718E-34    # Reduced Planck     Js
+const ħ = 1.0545718E-34     # Reduced Planck     Js
 const me = 9.10938356E-31   # Electron Mass      kg
 const ee = 1.6021766208E-19 # Elementary Charge  C
 const ϵ0 = 8.854187817E-12  # Vaccum Permitivity F/m
 const Kb = 1.38064852E-23   # Boltzmann Constant J/K
-const T  = 300              # ~Room Temperature  Ka generalized theory of flexa
+const T  = 300              # ~Room Temperature  K
 
 # Indices of refraction/dispersion for Argon 1/nm
 const C1 = 0.012055E3
@@ -108,9 +108,9 @@ l1min=λ_tot(abs(lambda_tot_rouge-lmin)==min(abs(lambda_tot_rouge-lmin)));
 l1max=λ_tot(abs(lambda_tot_rouge-lmax)==min(abs(lambda_tot_rouge-lmax)));
 This should do it I believe:=#
 λ_test = minimum(abs.(λ_tot-λ_min))
-const λ1_min = filter(x -> abs(x - λ_min) == λ_test, λ_tot)[1]
+const λ_min = filter(x -> abs(x - λ_min) == λ_test, λ_tot)[1]
 λ_test = minimum(abs.(λ_tot-λ_max))
-const λ1_max = filter(x -> abs(x - λ_max) == λ_test, λ_tot)[1]
+const λ_max = filter(x -> abs(x - λ_max) == λ_test, λ_tot)[1]
 
 const ρ_crit = ω^2*me*ϵ0/ee^2
 const k_Ar   = ceil.(Ui_Ar ./ (hb*ωω))
@@ -196,11 +196,11 @@ function plasma(dt, α, ρ_at, Potentiel_Ar, E, C2_Ar, Nt)
     return ρ_Ar
 end
 
-function prop_lin(deriv_t_2, dz1, E, losses)
+function prop_lin(E, deriv_t_2, dz, losses)
     # Shift to frequency domain and compute linear propagation
-    E_TF = fftshift(fft(fftshift(E))) .* exp.(1im*(deriv_t_2)*dz1)
+    E_TF = fftshift(fft(fftshift(E))) .* exp.(1im*(deriv_t_2)*dz)
     # Shift back to time domain, compute losses and return
-    return ifftshift(ifft(ifftshift(E_TF))).*exp.(-losses/2 * dz1)
+    return ifftshift(ifft(ifftshift(E_TF))).*exp.(-losses/2 * dz)
 end
 
 function prop_non_lin(E, rrr, ρ, dz, losses, kerr_response)
@@ -219,10 +219,10 @@ function calc_compression(width, Cs)
                      Cs[5]*(λ_tot).^2./((λ_tot).^2-Cs[6]^2))
 
     n_begin = n_fs[findfirst(x->x==λ_begin, λ_tot)]
-    n_fs[find(x->x<λ_begin, λ_tot)] = n_begin
+    n_fs[λ_tot.<λ_begin] = n_begin
 
     n_final = n_fs[findfirst(x->x==λ_final, λ_tot)]
-    n_fs[find(x->x>λ_final, λ_tot)] = n_final
+    n_fs[λ_tot.>λ_final] = n_final
 
     spline = Spline1D(ωω_tot, n_fs)
     n_fs_interpd = evaluate(n_fs_spline, ωω_tot)
@@ -247,24 +247,70 @@ function smooth(values, radius)
     return smoothed_values
 end
 
-function steepening(E1, idxp, idxn, γ_2, γ_4, γ_6, γ_8, γ_10, ω_0, dt, dz)
-    NL = (γ_2 * abs.(E1)^2 + γ_4 * abs.(E1)^4 + γ_6 * abs.(E1)^6 +
-          γ_8 * abs.(E1)^8 + γ_10 * abs.(E1)^10) * dz
-    Temp = NL .* E1
+function steepening(E, idxp, idxn, γs , ω_0, dt, dz)
+    NL = (γ[1] * abs.(E)^2 + γ[2] * abs.(E)^4 + γ[3] * abs.(E)^6 +
+          γ[4] * abs.(E)^8 + γ[5] * abs.(E)^10) * dz
+    Temp = NL .* E
     k1 = (im / ω_0) * ((Temp[idxp] - Temp[idxn])/(2 * dt))
-    E1 = E1 + (0.5 * k1)
+    E = E + (0.5 * k1)
 
-    NL = (γ_2 * abs.(E1)^2 + γ_4 * abs.(E1)^4 + γ_6 * abs.(E1)^6 +
-          γ_8 * abs.(E1)^8 + γ_10 * abs.(E1)^10) * dz
-    Temp = NL .* E1
+    NL = (γ[1] * abs.(E)^2 + γ[2] * abs.(E)^4 + γ[3] * abs.(E)^6 +
+          γ[4] * abs.(E)^8 + γ[5] * abs.(E)^10) * dz
+    Temp = NL .* E
     k2 = (im / ω_0) * ((Temp[idxp] - Temp[idxn])/(2 * dt))
 
-    return E1 + k2
+    return E + k2
+end
+
+function calc_ks(ωω, λ_tot, ωω_tot, n_tot)
+    # Interpolation of n
+    n_interp = Spline1D(ωω_tot, n_tot)
+    n_tot = evaluate(n_interp, ωω_tot)
+    dn    = derivative(n_interp, ωω_tot, nu=1)
+    d2n   = derivative(n_interp, ωω_tot, nu=2)
+    d3n   = derivative(n_interp, ωω_tot, nu=3)
+    d4n   = derivative(n_interp, ωω_tot, nu=4)
+
+    # Frequency Disperion
+    k_tot = n_tot .* ωω_tot/c
+    k_tot[λ_tot .< 245] = max(k_tot)
+
+    k_first  = 1/c * (dn .* ωω_tot + n_tot)
+    k_second = 1/c * (d2n .* ωω_tot + 2 * dn)
+    k_third  = 1/c * (d3n .* ωω_tot + 3 * d2n)
+    k_fourth = smooth(1/c * (d4n .* ωω_tot + 4 * d3n), 100)
+
+    k  = k_tot[find_first(x->x==ωω,ωω_tot)]
+    k1 = k_first[find_first(x->x==ωω,ωω_tot)]
+    k2 = k_second[find_first(x->x==ωω,ωω_tot)]
+    k3 = k_third[find_first(x->x==ωω,ωω_tot)]
+    k4 = k_fourth[find_first(x->x==ωω,ωω_tot)]
+    return [k,k1,k2,k3,k4]
+end
+
+function calc_ns(pressure, n, n_tot, λ_tot)
+    n2_800  = 1e-23   * pression_z
+    n4_800  =-3.7e-42 * pression_z
+    n6_800  = 4e-58   * pression_z
+    n8_800  =-1.7e-75 * pression_z
+    n10_800 = 8.8e-94 * pression_z
+
+    n_800 = n_tot[abs.(λ_tot - 800) == minimum(abs.(λ_tot-800))]
+    n2  = n2_800 * ((n^2 - 1)/(n_800^2-1))^4
+    n4  = n2_800 * ((n^2 - 1)/(n_800^2-1))^6
+    n8  = n2_800 * ((n^2 - 1)/(n_800^2-1))^10
+    n10 = n2_800 * ((n^2 - 1)/(n_800^2-1))^12
+
+    return [n,n2,n4,n8,n10]
 end
 
 function plasma_potential(E,ω,Zeff,Ui)
-    Uh = 13.5984*ee
-    ω_au = 4.1E16
+    """
+    Derived From PPT Theory
+    http://jetp.ac.ru/cgi-bin/dn/e_023_05_0924.pdf
+    """
+    Uh = 13.5984*ee                     # Hydrogen Ionization Potential
+    ω_au = 4.1E16                       # Ionization Potential (1/s Natural)
     γ = ω .* sqrt(2*me*Ui)./(ee*E)
     Eh = ee^5*me^2/(ħ^4 * (4*π*ϵ0)^3)
     E0 = Eh * (Ui/Uh) ^ (3/2)
@@ -289,7 +335,8 @@ function plasma_potential(E,ω,Zeff,Ui)
         A += 4/sqrt(3*π) * γ.^2 ./ (1+γ.^2) * exp(-α * (z-nu)) * dawson(sqrt(abs(beta*(z-nu))))
     end
 
-    potential = ω_au * C_nl2 * f
+    potential = ω_au * C_nl2 * f * sqrt(6/π) * (Ui / (2 * Uh)) * A .* (2 * E0/(E .* sqrt.(1+γ.^2))).^(2 * n⋆ - abs(m) - 3/2) .* exp.(-2 * E0 * g ./ (3*E))
+    potential[isnan.(potential)]=0
 end
 
 #=
@@ -299,6 +346,73 @@ end
 ██  ██  ██ ██   ██ ██ ██  ██ ██
 ██      ██ ██   ██ ██ ██   ████
 =#
-#while z <= zmax
-#    pressure = calc_pressure(0.008, Pressure, z, zmax)
-#end
+
+#Temporary Flag
+run = false
+while z <= zmax && run
+    # Calculate Pressure
+    pressure_z = calc_pressure(0.008, Pressure, z, zmax)
+    push!(pressure, pressure_z)
+
+    # Update of n, with cutoffs
+    n_tot = sqrt(1+pressure_z * (n_tot_0.^2 - 1))
+    n_max = n_tot[findfirst(λ->λ==λ_max, λ_tot)]
+    n_tot[λ_tot .> λ_max] = n_max
+    n_min = n_tot[findfirst(λ->λ==λ_min, λ_tot)]
+    n_tot[λ_tot .< λ_min] = n_min
+
+    ks = calc_ks()
+    n  = n_tot[find_first(x->x==ωω,ωω_tot)]
+    vg = 1/ks[2]
+
+    # Argon Parameters
+    ns = calc_ns()
+    β2 = pressure_z * k[3]
+    β3 = pressure_z * k[4]
+    β4 = pressure_z * k[5]
+    τ = 3.5E-13 / pressure_z
+    ρ_at = pressure_z * 1E5 / (kB * T)
+
+    # Plasma Parameters
+    σ_k = 2.81E-96 * pressure
+    σ   = (k[1]*ee^2) / (ωω * m * ϵ0) * τ/(1+(ωω * τ).^2)
+    β_k = 10^(-4 * k_Ar) * k_Ar * ħ * ωω * ρ * 0.21 * σ_k
+    rrr = -im * k[1]/(2 * n[1]^2 * ρ_crit) - 0.5 * σ
+    coeff2 = σ/Ui_Ar
+
+    # Dispersion and Laplacian Operators
+    dv_t_2_op = k_tot - k[1] - k[2] * ωω
+
+    # Kerr factors
+    γs = im * n[2:end] * k[0]/n[0]
+
+    # Propagation
+    E = prop_lin(E, dv_t_2_op, dz, losses)        #Linear
+    E = steepening(E, idxp, idxn, γs, ωω, dt, dz) #Steepening
+
+    # Plasma
+    U_ion = PPT(abs(E).^2, c, ωω)
+    ρ = plasma(dt, α, ρ_at, U_ion, E, coeff2, Nt)
+    plasma_loss = U_ion / (2 * abs.(E).^2) * Ui_Ar * (ρ_at - ρ) * dz
+    plasma_loss[isnan.(plasma_loss)] = 0
+
+    # Kerr and Plasma Propagation (NonLinear)
+    kerr_response = -(γs[1]*(abs.(E)).^2 + γs[2]*(abs.(E)).^4 + γs[3] * abs.(E).^6 + γs[4]*abs.(E).^8 + γ[5].*abs.(E).^10) * dz
+    E = prop_non_lin(E, rrr, ρ, dz, plasma_loss, kerr_response)
+
+    # Update Distances
+    ZZZ += dz
+    z   += dz
+    distance = 100*ZZZ
+    dist[1, JJJ+1] = z
+
+    # Calculating Results
+    I_max[JJJ+1]         = maximum(abs.(E).^2)
+    It_dist[JJJ+1]       = abs.(E).^2
+    spectrum_dist[JJJ+1] = abs.(fftshift(fft(fftshift(E)))).^2
+    ΔT_pulsa[JJJ+1]      = calc_duration(E,t)
+    ρ_max[JJJ+1]         = maximum(ρ*1E-6)
+
+    #TODO: Save Intermediate Results
+    JJJ += 1
+end
