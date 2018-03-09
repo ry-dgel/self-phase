42822d3356f51ede881a5dd4e392c4c88150ea12
Rigel Zifkin <rydgel.code@gmail.com>
2018-03-09 18:51:44 -0500

    Redid old changes

--- Analysis/fiber_set.py
+++ Analysis/fiber_set.py
@@ -6,18 +6,19 @@ class fiber_set:
         self.runs = []
         for root, folders, files, in os.walk(data_folder):
             for folder in folders:
-                data_folder = os.join.path(root, folder)
-                self.runs.append(fiber_run(data_folder))
+                print("Loading data from %s" % folder, end='r')
+                data_folder = os.path.join(root, folder)
+                self.runs.append(fiber_run.fiber_run(data_folder))
 
     def maxmetric(self, metric):
         maximum = max(map(lambda run : run.metrics[metric], self.runs))
-        return maxium, next(run for run in self.runs if 
-                            self.runs.metrics[metric] == maximum)
+        return maximum, next(run for run in self.runs if 
+                            run.metrics[metric] == maximum)
 
     def minmetric(self, metric):
         minimum = min(map(lambda run : run.metrics[metric], self.runs))
         return minimum, next(run for run in self.runs if
-                             self.runs.metrics[metric] == minimum)
+                             run.metrics[metric] == minimum)
 
     def constparam(self, param, value):
         return [run for run in self.runs if self.runs.params[param] == value]
