160283413bb87cb5337fce730e460a959d64af3e
Rigel Zifkin <rydgel.code@gmail.com>
2018-03-09 21:36:14 -0500

    metrics now actually calculates widths

--- Analysis/metrics.py
+++ Analysis/metrics.py
@@ -2,8 +2,9 @@ import numpy as np
 from numpy import fft as f 
 
 def populate_metrics(fiber_run, fname):
-    bw = bandwidth(fiber_run.fields[-1])
-    pw = pulse_width(fiber_run.fields[-1])
+    params = fiber_run.params
+    bw = bandwidth(params, fiber_run.fields[-1])
+    pw = pulse_width(params, fiber_run.fields[-1])
     pr = power_ratio(fiber_run.fields[-1], fiber_run.fields[0])
     rho_max = plas_denzel(fname)
     metrics = {"pwidth":pw, "bwidth":bw[0], "l_edge":bw[1], "r_edge":bw[2],
@@ -15,19 +16,26 @@ def plas_denzel(fname):
     rho_max = max(plas_denzel)
     return rho_max	
 
-def bandwidth(E):
+def bandwidth(params, E):
     Ef = f.fftshift(f.fft(f.fftshift(E)))
     If = np.power(np.abs(Ef),2)
+
+    freq = np.arange(-params["Nt"]/2, params["Nt"]/2, 1)/params["tmax"]
+    freq = freq + 299792458/params["lambda"]
+
     tm = 0.1*max(If)
-    right_edge = If[len(If)-np.argmax(np.flip(If,0) > tm)]
-    left_edge = If[np.argmax(If > tm)]
+    right_edge = freq[len(If)-np.argmax(np.flip(If,0) > tm)]
+    left_edge = freq[np.argmax(If > tm)]
     bw = right_edge - left_edge
     return [bw, left_edge, right_edge]
 
-def pulse_width(E):
+def pulse_width(params, E):
     I = np.power(np.abs(E),2)
+
+    time = np.arange(-params["Nt"]/2, params["Nt"]/2, 1) * params["dt"]
+
     tm = 0.1*max(I)
-    pw = I[len(I) - np.argmax(np.flip(I,0) > tm)] - I[np.argmax(I > tm)]
+    pw = time[len(I) - np.argmax(np.flip(I,0) > tm)] - time[np.argmax(I > tm)]
     return pw
 
 def power_ratio(E, E0):
