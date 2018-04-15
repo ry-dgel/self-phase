7d27dd73cf31286e8f1ec13c41348be2efe5b637
Rigel Zifkin <rydgel.code@gmail.com>
2018-04-15 18:03:24 -0400

    Proper mutlthreading

--- MultiThread.jl
+++ MultiThread.jl
@@ -1,8 +1,9 @@
-@everywhere include("self-phase.jl")
-using YAML
+push!(LOAD_PATH, pwd())
+@everywhere using SelfPhase
 using Iterators
+using ProgressMeter
+using YAML
 #=
-"""
 To run, simply use:
 ```
 julia multi-thread.jl <parameter file> <#saves>
@@ -11,26 +12,73 @@ e.g:
 ```
 julia multi-thread.jl params.yaml 10
 ```
-"""
+=#
 
-"""
-    unpack(p, numSaves)
+#=
+    runSim(p, numSaves)
 
 given a dictionary of inital paremeters, and a number of times to save, performs
 the entire simulation with those parameters.
-"""
 =#
-@everywhere function unpack(p, numSaves)
+
+globalProgressMeters = Dict()
+globalProgressValues = Dict()
+globalPrintLock = Dict()
+
+function Base.pmap(f::Function, p::Progress, values...; kwargs...)
+    global globalProgressMeters
+    global globalProgressValues
+    global globalPrintLock
+
+    id = randstring(50)
+    globalProgressMeters[id] = p
+    globalProgressValues[id] = 0
+    globalPrintLock[id] = ReentrantLock()
+
+    passcallback = false
+    kwa = Dict(kwargs)
+    if haskey(kwa,:passcallback)
+      passcallback = true
+      delete!(kwa,:passcallback)
+    end
+
+    out = pmap(values...; kwa...) do x...
+        if passcallback
+          v = f(n -> remotecall(updateProgressMeter, 1, id, n), x...)
+        else
+          v = f(x...)
+          wait(remotecall(updateProgressMeter, 1, id, 1))
+        end
+        v
+    end
+
+    delete!(globalProgressMeters, id)
+    out
+end
+
+#"This is remote-called by all the workers to update the progress."
+@everywhere function updateProgressMeter(id,n)
+    global globalProgressMeters
+    global globalProgressValues
+    global globalPrintLock
+
+    lock(globalPrintLock[id])
+    globalProgressValues[id] += n
+    update!(globalProgressMeters[id] , globalProgressValues[id])
+    unlock(globalPrintLock[id])
+end
+
+@everywhere function runSim(p, numSaves)
     # Generate folder string
-    fname = @sprintf("%.0fnm_%.0fμJ_%.2fbar_%.2fbar_%.0ffs_%.1fm_%.0ffs^2_%.0fμm",
-                     p["λ"]*1E9, p["Energy"]*1E6, p["Pin"], p["Pout"], p["Tfwhm"]*1E15,
+    fname = @sprintf("%.0fnm_%.0fμJ_%.2fbar_%.0ffs_%.1fm_%.0ffs^2_%.0fμm",
+                     p["λ"]*1E9, p["Energy"]*1E6, p["Pin"], p["Tfwhm"]*1E15,
                      p["zmax"], p["Chirp"], p["fiberD"]*1E6)
     # Derive additional constants and spatial grids
     derive_constants(p)
     # Initialize electric field
     E, zinit = initialize(fname, p, "resume" in ARGS, "keep" in ARGS)
     # Save inital data
-    saveData(fname, E, 0, zinit)
+    saveData(fname, E, 0, 0, zinit)
     # Simulate the whole thing
     simulate(E, p, zinit, fname, numSaves)
 end
@@ -59,15 +107,19 @@ param_tuples = collect(product(values(lists)...))
 if "data" ∉ readdir()
     mkdir("data")
 end
-# Put all processes in same place.
-@everywhere cd("data")
+@everywhere cd("data") # Must be run on every thread
 
-if length(ARGS) < 2
-    println("No number of saves passed, assuming 2.")
-    numSaves = 2
-else
+if length(ARGS) > 2
     numSaves = parse(Int, ARGS[2])
+else
+    println("No number of saves passed, assuming 2")
+    numSaves = 2
 end
 
+print("Starting Simulations\n")
+prog = Progress(length(param_tuples), 5, "Running Parallel Simulations...")
 # Parallel map over all paramters to simulate!
-pmap(jawn -> unpack(Dict{Any,Any}(zip(keys(lists), jawn)), numSaves), param_tuples)
+pmap(jawn -> runSim(Dict{Any,Any}(zip(keys(lists), jawn)), numSaves),
+     prog,
+     param_tuples)
+ProgressMeter.finish!(prog)
